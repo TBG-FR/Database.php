b0987f8d269ff843ab23fbc54a8cb5ed4bd882f7
Tom-Brian Garcia <tom-brian.garcia@etu.univ-lyon1.fr>
2017-11-11 19:25:33 +0100

    Database.php - Placeholder

---
+++ Database.php
@@ -0,0 +1,4 @@
+// My own version of that 'Database' class, merging both versions that are located in https://github.com/TBG-FR/Database.php/Ressources/
+// The aim of that one is to put together the benefits of both version, and to be well-commented and upgraded
+
+// Copy its code here
