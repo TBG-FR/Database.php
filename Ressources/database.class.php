a5a8f0d4aa1ee069287363beffa4b315a3c7c702
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-11-11 21:00:37 +0100

    Ressources - Cleaning

--- Ressources/database.class.php
+++
@@ -1,3 +0,0 @@
-// The 'database.class.php' class made by Mr. Philip Brown on the post "Roll your own PDO PHP Class" (http://culttt.com/2012/10/01/roll-your-own-pdo-php-class/)
-
-// Copy its code here
