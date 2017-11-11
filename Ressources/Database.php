692f82875ea80b0855445fbec0ea4f6bebc7b1c6
Tom-Brian Garcia <tom-brian.garcia@etu.univ-lyon1.fr>
2017-11-11 19:21:40 +0100

    Ressources : Database.php (Correction TP3 PHP)

---
+++ Ressources/Database.php
@@ -0,0 +1,3 @@
+// The 'Database' class made by Mr. Robin Frere (https://www.widee.fr/) for Computer Science students at Lyon1 University (Bourg-en-Bresse) as the correction of a practical work (TP3) in PHP Course
+
+// Copy its code here
